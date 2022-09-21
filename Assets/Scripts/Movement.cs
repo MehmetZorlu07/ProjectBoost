42150cc54a5c1c33e93962b2391f266222239d42
Mehmet Zorlu <memo.zzzorlu@gmail.com>
2022-09-21 12:19:43 +0200

    Audio added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,11 +7,13 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationThrust = 1f;
     Rigidbody rb;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,7 +26,13 @@ public class Movement : MonoBehaviour
     void ProcessThrust() {
         if (Input.GetKey(KeyCode.Space)) {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if(!audioSource.isPlaying) {
+                audioSource.Play();
+            }
         }
+        else {
+                audioSource.Stop();
+            }
     }
 
     void ProcessRotation() {
